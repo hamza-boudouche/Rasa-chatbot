b652d202e9348d884220169392d61fc03cf3080c
hamza-boudouche <boudouche.hamza.13.07.2000@gmail.com>
2022-03-27 14:58:43 +0000

    get tasks intent + add event intent

--- actions/actions.py
+++ actions/actions.py
@@ -43,6 +43,42 @@ class ActionGetTask(Action):
 
     def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
         print("getting task")
+        employee = tracker.get_slot('employee')
+        print(employee)
+        time = tracker.get_slot("time")
+        print(time)
         dispatcher.utter_message(text="getting task")
+
+        return []
+
+
+class ActionConfirmAddEvent(Action):
+
+    def name(self) -> Text:
+        return "action_confirm_add_event"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("confirm adding event")
+        time = tracker.get_slot("time")
+        print(time)
+        end_time = tracker.get_slot("end_time")
+        print(end_time)
+        event_title = tracker.get_slot("event_title")
+        print(event_title)
+        description = tracker.get_slot("description")
+        print(description)
+
+        dispatcher.utter_message(text="confirm add event")
+        return []
+
+
+class ActionAddEvent(Action):
+
+    def name(self) -> Text:
+        return "action_add_event"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        print("adding event")
+        dispatcher.utter_message(text="adding event")
 
         return []
